dd6bde986fa89a29563480adf6d417571e08fd2e
Shomy <61943525+shomykohai@users.noreply.github.com>
2024-06-09 11:22:22 +0200

    Refactor dialogues using enums

--- quests/get_apple_quest.gd
+++ quests/get_apple_quest.gd
@@ -1,21 +1,24 @@
 extends Quest
 
 
+@export var needed_apple: int = 4
+
+
 func start():
 	pass
 
-func _update_objective() -> void:
-	objective_completed = !objective_completed
-
 func update():
-	if State.apple_count < 4:
+	if State.apple_count < needed_apple:
 		State.apple_count += 1
 	else:
 		State.apple_count += 1
-		State.apple_status = "has"
+		State.apple_status = State.APPLE_STATUS.HAS
+		# The quest objective will automatically be set to true when calling update
+		objective_completed = true
 
 
 func complete():
-	if State.apple_status == "has":
-		State.apple_status = "gave"
+	if State.apple_status == State.APPLE_STATUS.HAS:
+		State.apple_status = State.APPLE_STATUS.GAVE
+		State.apple_count = 0
 
--- state.gd
+++ state.gd
@@ -1,5 +1,12 @@
 extends Node
 
 
-var apple_status: String = ""
+enum APPLE_STATUS {
+	TREE, #No apples yet
+	HAS,
+	GAVE
+}
+
+
+var apple_status: APPLE_STATUS = APPLE_STATUS.TREE
 var apple_count: int = 0
